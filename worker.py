05935b686a95d8586ec5de1cd21688d46445bd39
john <jonathan@getrecruitmate.com>
2019-01-17 23:01:04 -0800

    WIP

--- worker.py
+++ worker.py
@@ -9,8 +9,26 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
+from rq import Queue
+from workerfunction import twitter_checker
+from rq_scheduler import Scheduler
+
+def queueing():
+    q = Queue('Default', connection=conn)
+    scheduler = Scheduler(queue=q)
+    print('scheduler executed')
+
+    scheduler.cron(
+        "*/5 * * * *",                # A cron string (e.g. "0 0 * * 0")
+        func=twitter_checker,                  # Function to be queued
+     #   args=[arg1, arg2],          # Arguments passed into function when executed
+     #   kwargs={'foo': 'bar'},      # Keyword arguments passed into function when executed
+        queue_name='Default'      # In which queue the job should be put in
+     #   meta={'foo': 'bar'}         # Arbitrary pickleable data on the job itself
+    )
+
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
-        queueing()
+        queueing(conn)
