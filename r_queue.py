c0edc744e528196bc25a7978da1d19691f274c75
john <jonathan@getrecruitmate.com>
2019-01-19 00:56:53 -0800

    testing queues more

--- r_queue.py
+++ r_queue.py
@@ -1,7 +1,8 @@
 #from rq import Queue
 #from worker import conn
-from workerfunction import twitter_checker
+#from workerfunction import twitter_checker
 from rq_scheduler import Scheduler
+from testfunction import count
 
 
 import django_rq
@@ -9,7 +10,7 @@ scheduler = django_rq.get_scheduler('default')
 print('scheduler has started')
 job = scheduler.cron(
         "*/5 * * * *",                # A cron string (e.g. "0 0 * * 0")
-        func=twitter_checker,                  # Function to be queued
+        func=count,                  # Function to be queued
      #   args=[arg1, arg2],          # Arguments passed into function when executed
      #   kwargs={'foo': 'bar'},      # Keyword arguments passed into function when executed
      #   repeat=10,                  # Repeat this number of times (None means repeat forever)
