a092ed5defac6c5ae1a1ffae827be01f45546470
john <jonathan@getrecruitmate.com>
2019-01-19 22:38:21 -0800

    modified queue again

--- editdojo_project/settings.py
+++ editdojo_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django_rq',
-    'dj_database_url',
     'scheduler',
 
     'django.contrib.sites',
