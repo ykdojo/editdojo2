11f03ab2552c5d0a511fbfe03398cccf33998dc3
john <jonathan@getrecruitmate.com>
2019-01-19 22:22:02 -0800

    modified queue again

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Language
-from .models import already_in_twitter
 
 # returns True if the user has already finished the signup flow.
 def finished_signup_flow(user):
@@ -83,20 +82,4 @@ def select_languages(request):
     current_user.save()
     return HttpResponseRedirect('/languagesSelected/')
 
-def twitter_checker():
-    print("twitter checker started")
-    #todo: find a way to keep tweepy authenticated without specifying it on every worker spinup
-    #todo: find a way to retrieve user id in django models so i dont have to run api to get user id
-    auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
-    auth.set_access_token(os.environ['TWITTER_ACCESS_KEY'], os.environ['TWITTER_ACCESS_SECRET'])
-    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
-    #to save a record, use already_in_twitter(already_in_twitter=str(current_user), user=False).save()
-    obj = already_in_twitter.objects.filter(already_in_twitter=False)
-    for i in range(len(obj)):
-        user_to_add = obj.values_list('user')[i][0]
-        user_to_add_id = api.get_user(user_to_add)
-        api.add_list_member(id=user_to_add_id, slug='testlist', owner='editdojo')
-        update_db = already_in_twitter.objects.get(user=user_to_add)
-        update_db.already_in_twitter = True
-        update_db.save()
 
