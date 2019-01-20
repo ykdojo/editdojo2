629168961adc5b9624ca97bb23ad4c28a5405ce5
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2019-01-20 16:00:59 -0500

    Reverting some unnecessary changes

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def my_view(request):
-    return render(request, 'hello.html')
+    return HttpResponse('Hello, World Ahhh!')	
 
 def home_view(request):
     return HttpResponse('This is home page')
