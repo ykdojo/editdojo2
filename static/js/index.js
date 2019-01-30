885d3ac9d504b8da120bee7e8984360351b27a9c
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2019-01-30 18:03:03 -0500

    Show when the tweets were posted

--- static/js/index.js
+++ static/js/index.js
@@ -2,17 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Cookies from 'js-cookie'
 
+function formatDate(dateString) {
+  const givenDate = new Date(dateString);
+  const timeSince = new Date(Date.now() - givenDate);
+
+  const days = timeSince.getDay();
+  if (days > 0) {
+    const toAppend = days == 1 ? ' day ago' : ' days ago'
+    return days + toAppend;
+  }
+
+  const hours = timeSince.getHour();
+  if (hours > 0) {
+    const toAppend = hours == 1 ? ' hours ago' : ' hours ago'
+    return hours + toAppend;
+  }
+
+  const minutes = timeSince.getMinute();
+  if (minutes > 0) {
+    const toAppend = minutes == 1 ? ' minutes ago' : ' minutes ago'
+    return minutes + toAppend;
+  }
+
+  return 'just posted';
+}
 
 class Post extends React.Component {
   render() {
     const post = this.props.data;
-    const date = post.date_posted;
+    const date = formatDate(post.date_posted);
     const profileLink = "https://twitter.com/intent/user?user_id=" + post.associated_social_account.uid;
     return (
         <div className="section" style={{padding: '5px 0', fontSize: '14px'}}>
           <div className='card-body' style={{padding: '15px 10px'}}>
             <div style={{fontSize: '15px', marginBottom: '10px', lineHeight: '20px'}}>
-              <a href={profileLink} target="_blank">@{post.posted_by.username}</a> - {date.toString()}
+              <a href={profileLink} target="_blank">@{post.posted_by.username}</a> - {date}
             </div>
             <div style={{lineHeight: '21px'}}>{post.text_content}</div>
           </div>
